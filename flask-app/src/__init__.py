e3ea55d1d61c96db912cf0fecdecb5b585289b56
Chapin <116377406+chapinalf@users.noreply.github.com>
2022-12-08 14:49:21 -0500

    Add files via upload

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -6,9 +6,10 @@ from flaskext.mysql import MySQL
 # create a MySQL object that we will use in other parts of the API
 db = MySQL()
 
+
 def create_app():
     app = Flask(__name__)
-    
+
     # secret key that will be used for securely signing the session 
     # cookie and can be used for any other security related needs by 
     # extensions or your application
@@ -19,19 +20,19 @@ def create_app():
     app.config['MYSQL_DATABASE_PASSWORD'] = open('/secrets/db_password.txt').readline()
     app.config['MYSQL_DATABASE_HOST'] = 'db'
     app.config['MYSQL_DATABASE_PORT'] = 3306
-    app.config['MYSQL_DATABASE_DB'] = 'classicmodels'  # Change this to your DB name
+    app.config['MYSQL_DATABASE_DB'] = 'menuette'
 
     # Initialize the database object with the settings above. 
     db.init_app(app)
-    
+
     # Import the various routes
-    from src.views import views
-    from src.customers.customers import customers
-    from src.products.products  import products
+    from src.owner.owner import owner
+    from src.blogger.blogger import blogger
+    from src.searcher.searcher import searcher
 
     # Register the routes that we just imported so they can be properly handled
-    app.register_blueprint(views,       url_prefix='/classic')
-    app.register_blueprint(customers,   url_prefix='/classic')
-    app.register_blueprint(products,    url_prefix='/classic')
+    app.register_blueprint(owner, url_prefix='/owner')
+    app.register_blueprint(blogger, url_prefix='/blogger')
+    app.register_blueprint(searcher, url_prefix='/searcher')
 
     return app
